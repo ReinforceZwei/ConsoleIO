21bcac85de4b52982568fd5aac732ca75b988159
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2020-04-15 14:00:08 +0800

    加入專案檔案。

---
+++ ConsoleIO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
